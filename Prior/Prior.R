50c188cc68896d1249fc19b67ac699637187f1a8
Rafael <rafael.2706@gmail.com>
2016-07-11 17:06:35 -0300

    prior

--- Prior/Prior.R
+++ Prior/Prior.R
@@ -1,37 +1,37 @@
-
-#require(LearnBayes)
-# Previous knowledge
+#Previous knowledge
 
 # Sample size
 nobs <- 100
-
-# 75% are spiral
+ftatooine <- 0.65
+# 70% are spiral
 
 #Prior
-alpha <- 1+0.75*100
-beta  <- 1+(1-0.75)*100
+alpha <- 1+ftatooine*nobs
+beta  <- 1+(1-ftatooine)*nobs
 
 #Plot prior
 x<-seq(.005, .995, length=500)
-y<-dbeta(x, alpha,beta)
-plot(x,y, type="l",ylab="prior",xlab=expression(f[spirals]),col="blue",lwd=3)
-abline(v=0.75,lty=3,lwd=2)
+y <-dbeta(x, alpha,beta)
+plot(x,y, type="l",ylab="prior",xlab=expression(f[Tatooine]),col="blue",lwd=3)
+abline(v=0.65,lty=3,lwd=2)
 
 prior <- dbeta(x,alpha,beta)
 
 # 20 obs 
+nobs_new <- 20
+ftatooine_new <- 0.75
 
-spirals <- 0.5*2000
-nospirals <- 0.5*2000
+tatooine <- ftatooine_new *nobs_new
+notatooine <- (1-ftatooine_new)*nobs_new
 
 x = seq(0.005, 0.995, length = 500)
-like <- dbeta(x,spirals+1,nospirals+1)
+like <- dbeta(x,tatooine+1,notatooine+1)
 plot(x,like,type="l",col="blue")
-abline(v=0.5,lty=3,lwd=2)
+abline(v=0.75,lty=3,lwd=2)
 
 
 
-post=dbeta(x,alpha+spirals, beta+nospirals)
+post=dbeta(x,alpha+tatooine, beta+notatooine)
 m=max(c(prior,like,post))
 plot(x,post,type="l", ylab="Density", lty=2, lwd=3,
 ylim=c(0,m),col="red")
@@ -42,21 +42,21 @@ lty=c(3,1,2), lwd=c(3,3,3), col=c("green","blue","red"))
   
 
 
-prior = c(76,26)
-# New observation
-# 50% are spirals
-# spirals  = 10
-# others = 10
-
-data <- c(10,10)
-
-triplot(prior,data,where="topright")
-
-
-# Larger data 5000
-
-data <- c(2500,2500)
-
-triplot(prior,data,where="topright")
-
-
+# Plot a mosaic 
+par(mfrow = c(2, 3))
+par(cex = 0.6)
+par(mar = c(3, 3, 2, 2), oma = c(1, 1, 1, 1))
+v_obs<-c(1,5,25,50,100,500)
+for(i in 1:6){
+n_spirals <-v_obs[i]*tatooine
+n_nospirals <- v_obs[i]*notatooine
+post=dbeta(x,alpha+n_spirals, beta+n_nospirals)
+like <- dbeta(x,n_spirals+1,n_nospirals+1)
+m=max(c(prior,like,post))
+plot(x,post,type="l", ylab="Density", lty=2, lwd=3,
+     ylim=c(0,m),col="red")
+lines(x,like,lty=1, lwd=3,col="blue")
+lines(x,prior,lty=3, lwd=3,col="green")
+legend("topleft",c("Prior","Likelihood","Posterior",paste("Nobs=",v_obs[i]*tatooine+v_obs[i]*notatooine,sep="")),
+       lty=c(3,1,2,1), lwd=c(3,3,3,0), col=c("green","blue","red"))
+}
